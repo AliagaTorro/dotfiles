a0250d0fc5258360b42329b683146d01cb22aef8
Karulosu <carlosaliaga023@gmail.com>
2022-12-29 17:01:38 +0100

    UPDATE: Lazy configuration on plugins

--- nvim/lua/karulosu/plugins.lua
+++ nvim/lua/karulosu/plugins.lua
@@ -2,13 +2,24 @@ require("lazy").setup({
     "wbthomason/packer.nvim",
     -- THEME
     "ellisonleao/gruvbox.nvim",
-    "folke/tokyonight.nvim",
+    {
+        "folke/tokyonight.nvim",
+        lazy = false
+    },
+
+    {
+        "kyazdani42/nvim-web-devicons",
+        event = 'BufRead'
+    },
     -- STATUSLINE
-    "nvim-lualine/lualine.nvim",
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        dependencies = { 'kyazdani42/nvim-web-devicons' },
+    },
     "ThePrimeagen/harpoon",
     'phaazon/mind.nvim',
         -- DEPENDENCES
-    "kyazdani42/nvim-web-devicons",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     "kevinhwang91/promise-async",
@@ -47,75 +58,76 @@ require("lazy").setup({
         build = "make",
     },
 
-        -- LSP
-        "neovim/nvim-lspconfig",
-        "onsails/lspkind-nvim",
-        "jose-elias-alvarez/null-ls.nvim",
+    -- LSP
+    "neovim/nvim-lspconfig",
+    "onsails/lspkind-nvim",
+    "jose-elias-alvarez/null-ls.nvim",
 
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "j-hui/fidget.nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "j-hui/fidget.nvim",
 
-        -- GIT
-        "lewis6991/gitsigns.nvim",
-        "sindrets/diffview.nvim",
+    -- GIT
+    "lewis6991/gitsigns.nvim",
+    "sindrets/diffview.nvim",
 
-        -- COLORS
-        'NvChad/nvim-colorizer.lua',
-        -- TODO COMMENTS
-{
-            "folke/todo-comments.nvim", 
-            dependencies = "nvim-lua/plenary.nvim",
+    -- COLORS
+    'NvChad/nvim-colorizer.lua',
+    -- TODO COMMENTS
+    {
+        "folke/todo-comments.nvim", 
+        dependencies = "nvim-lua/plenary.nvim",
     },
 
-        -- AUTOCOMPLETION
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
-{
-        "hrsh7th/cmp-buffer", dependencies="nvim-cmp",
+    -- AUTOCOMPLETION
+    {
+    "hrsh7th/nvim-cmp",
+    event = 'InsertEnter',
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+            "saadparwaiz1/cmp_luasnip",
+        },
     },
-{
-        "hrsh7th/cmp-path", dependencies="nvim-cmp",
-    },
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
 
-        -- Snipets
-        "rafamadriz/friendly-snippets",
-        -- Surronding
-        "kylechui/nvim-surround",
+    -- Snipets
+    "rafamadriz/friendly-snippets",
+    -- Surronding
+    "kylechui/nvim-surround",
 
-        -- Comments
-        "numToStr/Comment.nvim",
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        -- Improve ui
-        "stevearc/dressing.nvim",
-        -- Tree
-        {
-            "nvim-neo-tree/neo-tree.nvim",
-            branch = "v2.x",
-            dependencies = { 
-                "nvim-lua/plenary.nvim",
-                "MunifTanjim/nui.nvim",
-        }
+    -- Comments
+    "numToStr/Comment.nvim",
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    -- Improve ui
+    "stevearc/dressing.nvim",
+    -- Tree
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        dependencies = { 
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
     },
 
-        -- REMOVE WAITING TIME
-        "max397574/better-escape.nvim",
-        -- CONTEXT AND CHARACTERS
-        "lukas-reineke/indent-blankline.nvim",
-        -- LSP SYMBOLS OF THE DOC
-        "simrat39/symbols-outline.nvim",
-        "danymat/neogen",
+    -- REMOVE WAITING TIME
+    "max397574/better-escape.nvim",
+    -- CONTEXT AND CHARACTERS
+    "lukas-reineke/indent-blankline.nvim",
+    -- LSP SYMBOLS OF THE DOC
+    "simrat39/symbols-outline.nvim",
+    "danymat/neogen",
 
-        -- DEBUGGER
-        "mfussenegger/nvim-dap",
-        "rcarriga/nvim-dap-ui",
-        "theHamsta/nvim-dap-virtual-text",
+    -- DEBUGGER
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
 
-        -- LANG SPECIFIC
+    -- LANG SPECIFIC
 
-        -- FLUTTER
-        "akinsho/flutter-tools.nvim",
+    -- FLUTTER
+    "akinsho/flutter-tools.nvim",
 })
